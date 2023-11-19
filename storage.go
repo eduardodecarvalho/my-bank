a2a027db85f0126d0df5960214008c54da2717f5
decarvalho <decarvalho.dev@gmail.com>
2023-11-19 22:14:30 +0100

    refact

--- storage.go
+++ storage.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "database/sql"
+	"database/sql"
 
-  _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 type Storage interface {
-  CreateAccount(*Account) error
-  DeleteAccount(int) error
-  UpdateAccount(*Account) error
-  GetAccountByID(int) (*Account, error)
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
+	UpdateAccount(*Account) error
+	GetAccountByID(int) (*Account, error)
 }
 
 type PostgresStore struct {
-  db *sql.DB
+	db *sql.DB
 }
 
 func NewPostgresStore() (*PostgresStore, error) {
-  connStr := "user=postgres dbname=postgres password=password sslmode=disable"
-  db, err := sql.Open("postgres", connStr)
-  if err != nil {
-    return nil, err
-  }
-  if err := db.Ping(); err != nil {
-    return nil, err
-  }
-  
-  return &PostgresStore{
-    db: db,
-  }, nil
+	connStr := "user=postgres dbname=postgres password=password sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{
+		db: db,
+	}, nil
 }
 
 func (s *PostgresStore) Init() error {
-  return nil
+	return nil
 }
 
 func (s *PostgresStore) CreateAccountTable() error {
-  query := `create table if not exists ACCOUNT (
+	query := `create table if not exists ACCOUNT (
     id serial primary key,
     first_name varchar(50),
     last_name varchar(50),
@@ -46,23 +46,22 @@ func (s *PostgresStore) CreateAccountTable() error {
     created_at timestamp,
   )`
 
-  _, err := s.db.Exec(query)
-  return err
+	_, err := s.db.Exec(query)
+	return err
 }
 
 func (s *PostgresStore) CreateAccount(*Account) error {
-  return nil
+	return nil
 }
 
 func (s *PostgresStore) UpdateAccount(*Account) error {
-  return nil
+	return nil
 }
 
 func (s *PostgresStore) DeleteAccount(id int) error {
- return nil
+	return nil
 }
 
 func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
- return nil, nil
+	return nil, nil
 }
-
